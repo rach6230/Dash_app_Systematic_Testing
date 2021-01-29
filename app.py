2ee5b6ef37925e83dedb5da79f7a1bd8cb83e36e
rach6230 <60782619+rach6230@users.noreply.github.com>
2021-01-29 15:56:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -57,9 +57,18 @@ app.layout = html.Div(children=[
   Output('click-data', 'children'),
   Input('graph-with-slider', 'clickData')
 )
+@app.callback(
+  Output('click-data', 'children'),
+  Input('graph-with-slider', 'clickData'))
 def display_click_data(clickData):
-  clickData
-  A = "hi"
+  if clickData == None:
+    A = "Select data point"
+  else:
+    temp = clickData['points'][0]['y']
+    lp = clickData['points'][0]['x']
+    ld = clickData['points'][0]['z']
+    vnt = clickData['points'][0]['marker.color']
+    A = 'Temperature ={}°C, Laser Power = {}μW, Laser Detuning = {}GHz, V/nT = {}'.format(temp, lp, ld, vnt)
   return A
 
 if __name__ == '__main__':
