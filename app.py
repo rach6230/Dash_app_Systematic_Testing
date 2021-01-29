7f34b1358a80fcdcb6e46c6b31a73430b7fbe38f
rach6230 <60782619+rach6230@users.noreply.github.com>
2021-01-29 14:27:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,11 @@ import pandas as pd
 import dash_table
 from dash.dependencies import Input, Output
 import plotly.express as px
+
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
 mytitle='Beer Comparison'
 tabtitle='SERF: Systematic Testing'
 myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
 
 #### Import Fit Data
 ALL_data_fit_values = pd.read_csv('https://raw.githubusercontent.com/rach6230/Dash_app_Systematic_Testing/main/Full_fit_Data.csv')
@@ -27,25 +21,7 @@ df2 = ALL_data_fit_values
 ######
 fig = px.scatter_3d(df2, x='Temp', y='Laser_Detuning', z='Laser_Power',
                    color='V/nT')
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
