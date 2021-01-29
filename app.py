e8810896c67ffb0954eecaa5b378935a6b29bcfb
rach6230 <60782619+rach6230@users.noreply.github.com>
2021-01-29 15:18:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -55,12 +55,12 @@ app.layout = html.Div(children=[
 def display_click_data(clickData):
   if clickData == None:
     A = "Select data point"
-  else:
-    temp = clickData['points'][0]['y']
-    lp = clickData['points'][0]['x']
-    ld = clickData['points'][0]['z']
-    vnt = clickData['points'][0]['marker.color']
-    A = 'Temperature ={}°C, Laser Power = {}μW, Laser Detuning = {}GHz, V/nT = {}'.format(temp, lp, ld, vnt)
+else:
+  temp = clickData['points'][0]['y']
+  lp = clickData['points'][0]['x']
+  ld = clickData['points'][0]['z']
+  vnt = clickData['points'][0]['marker.color']
+  A = 'Temperature ={}°C, Laser Power = {}μW, Laser Detuning = {}GHz, V/nT = {}'.format(temp, lp, ld, vnt)
 return A
 
 if __name__ == '__main__':
